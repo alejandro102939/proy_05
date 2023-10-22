593fcdcc736f6a90cbbbd77fd1be03b8d3a4dfd4
Alejandro102939 <moralesluis200504@gmail.com>
2023-10-22 12:41:32 -0500

    Primer commit

--- app.js
+++ app.js
@@ -4,25 +4,26 @@ const functions = require('./proy_modules/functions_texto.js')
 
 const main = async() => {
 
-        console.log('*************************************************'.cyan);
-        console.log('*            '.cyan, 'MAYUSCULAS'.bgYellow,'                 *'.cyan);
-        console.log('*************************************************'.cyan);
-        console.log('*            '.cyan, "" + functions.mayusculas('alejandro'),'                  *'.cyan);
-        console.log('*************************************************'.cyan);
-        console.log('*            '.cyan, 'MINUSCULAS'.bgYellow,'                 *'.cyan);
-        console.log('*************************************************'.cyan);
-        console.log('*            '.cyan, "" + functions.minusculas('LUIS'),'                      *'.cyan);
-        console.log('*************************************************'.cyan);
-        console.log('*************************************************'.cyan);
-        console.log('*            '.cyan, 'MAYUSCULAS Y MINUSCULAS'.bgYellow,'                   *'.cyan);
-        console.log('*************************************************'.cyan);
-        console.log('*            '.cyan, "" + functions.mayusculasyminusculas('LUIS', 'ALEJANDRO'),'                 *'.cyan);
-        console.log('*************************************************'.cyan);
-        console.log('*************************************************'.cyan);
-        console.log('*            '.cyan, 'SE QUITA LA ULTIMA LETRA'.bgYellow,'                  *'.cyan);
-        console.log('*************************************************'.cyan);
+        console.log('*************************************************'.red);
+        console.log('*            '.red, 'MAYUSCULAS'.bgYellow,'                 *'.red);
+        console.log('*************************************************'.red);
+        console.log('*            '.red, "" + functions.mayusculas('alejandro'),'                  *'.red);
+        console.log('*************************************************'.red);
+        console.log('*            '.red, 'MINUSCULAS'.bgYellow,'                 *'.red);
+        console.log('*************************************************'.red);
+        console.log('*            '.red, "" + functions.minusculas('LUIS'),'                      *'.red);
+        console.log('*************************************************'.red);
+        console.log('*************************************************'.red);
+        console.log('*            '.red, 'MAYUSCULAS Y MINUSCULAS'.bgYellow,'                   *'.red);
+        console.log('*************************************************'.red);
+        console.log('*            '.red, "" + functions.mayusculasyminusculas('LUIS', 'ALEJANDRO'),'                 *'.red);
+        console.log('*************************************************'.red);
+        console.log('*************************************************'.red);
+        console.log('*            '.red, 'SE QUITA LA ULTIMA LETRA'.bgYellow,'                  *'.red);
+        console.log('*************************************************'.red);
         let derecha = "luis alejandro";
 
+        // Funcion quitar la ultima letra una por una hasta quedar una letra 
         function quitarLetra1() {
             if (derecha.length > 0) {
                 derecha = derecha.slice(0, -1);
@@ -40,6 +41,7 @@ console.log('**************************************************'.cyan);
 
 let alreves = "luis alejandro";
 
+// Funcion quitar la primera letra una por una hasta quedar una letra
 function quitarLetra2() {
     if (alreves.length > 0) {
         console.log(alreves);
--- proy_modules/functions_texto.js
+++ proy_modules/functions_texto.js
@@ -1,11 +1,16 @@
 const functions = {};
 
+// Funcion de minusculas a MAYUSCULAS
 function mayusculas(alejandro) {
     return alejandro.toUpperCase();
 }
+
+// Funcion de MAYUSCULAS a minusculas 
 function minusculas(LUIS) {
     return LUIS.toLowerCase();
 }
+
+// Funcion primera letra en mayuscula en cada nombre, las demas letras en minusculas 
 function mayusculasyminusculas(alejandro, luis) {
     return alejandro[0].toUpperCase() + alejandro.substring(1).toLowerCase() +"  "+ luis[0].toUpperCase() + luis.substring(1).toLowerCase()
 }
